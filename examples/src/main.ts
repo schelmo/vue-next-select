aaebbdb36c6a0f30678e3ec5e69b7e0f26f66e76
Ernest <iendeavor.github.io@gmail.com>
2021-05-30 22:17:34 +0800

    feat: temp

--- examples/src/main.ts
+++ examples/src/main.ts
@@ -1,10 +1,10 @@
 import { createApp } from 'vue'
 // @ts-ignore-next
-import VueSelect from 'vue-next-select'
+import VueSelect from '../../src/index.vue'
 import App from './App.vue'
 import route from './route'
 import store from './store'
-import 'vue-next-select/dist/index.css'
+import '../../src/element-plus.css'
 import ApiReferenceLink from './components/api-reference-link.vue'
 
 createApp(App).component('vue-select', VueSelect).component('api', ApiReferenceLink).use(route).use(store).mount('#app')
